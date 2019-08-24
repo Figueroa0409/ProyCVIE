288f2c7be46ced0c0f7151d04e1f076e5fa2449f
Allan Figueroa <al_d_figueroa@hotmail.com>
2019-08-24 17:14:25 -0600

    Se arregla visualizacion de los indicadores, proceso de gráficas

--- Proyecto/Proyecto/Controllers/tasaBasicaPasivaController.cs
+++ Proyecto/Proyecto/Controllers/tasaBasicaPasivaController.cs
@@ -21,22 +21,24 @@ namespace Proyecto.Controllers
             DataTable dt = new DataTable();
             apiIndicadores.gee.wsindicadoreseconomicos api = new apiIndicadores.gee.wsindicadoreseconomicos();
             ds = api.ObtenerIndicadoresEconomicos("423", "01/08/2017", "01/08/2019", "Indicadores", "s", "smal11929@gmail.com", "AM9SAML30N");
-
+            var arrayFecha = new string[731];
+            var arrayValor = new string[731];
             try
             {
-                dt = ds.Tables[0];
-
-                for (int i = 0; i < dt.Rows.Count; i++)
+                dt = ds.Tables[0];               
+                for (int i = 0; i < 731; i++)
                 {
-                    ViewBag.FechaInicio = ("" + dt.Rows[i][1].ToString());
-                    ViewBag.Valor = ("" + dt.Rows[i][2].ToString());
+                    arrayFecha[i] = ("" + dt.Rows[i][1].ToString());
+                    arrayValor[i] = ("" + dt.Rows[i][2].ToString().Replace(",","."));                   
                 }
-
             }
             catch (Exception)
             {
 
             }
+            ViewBag.FechaInicio =  arrayFecha;
+            ViewBag.Valor = arrayValor;
+            ViewBag.Num = dt.Rows.Count;
             return View();
         }
 
