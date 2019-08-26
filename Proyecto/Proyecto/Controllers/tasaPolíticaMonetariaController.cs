8872d8331935dcf86e6011c5d48118dd346270c3
Sebas Alvarado <smal11929@gmail.com>
2019-08-25 18:17:21 -0600

    nombres

--- Proyecto/Proyecto/Controllers/tasaPolíticaMonetariaController.cs
+++ Proyecto/Proyecto/Controllers/tasaPolíticaMonetariaController.cs
@@ -21,14 +21,18 @@ namespace Proyecto.Controllers
             DataTable dt = new DataTable();
             apiIndicadores.gee.wsindicadoreseconomicos api = new apiIndicadores.gee.wsindicadoreseconomicos();
             ds = api.ObtenerIndicadoresEconomicos("3541", "01/08/2017", "01/08/2019", "Indicadores", "s", "smal11929@gmail.com", "AM9SAML30N");
-            var arrayFecha = new string[731];//731
+            string[] arrayFecha = new string[731];//731
             double[] arrayValor = new double[731];//731
             try
             {
                 dt = ds.Tables[0];
                 for (int i = 0; i < dt.Rows.Count; i++)
                 {
-                    arrayFecha[i] = (dt.Rows[i][1].ToString().Substring(0,9));
+                    DateTime fecha = Convert.ToDateTime(dt.Rows[i][1]);
+                    int año = fecha.Year;
+                    String mes = getMonth(fecha.Month);
+
+                    arrayFecha[i] = mes + " " + año;
                     arrayValor[i] = Convert.ToDouble(dt.Rows[i][2]);
                 }
             }
@@ -39,6 +43,61 @@ namespace Proyecto.Controllers
             ViewBag.FechaInicio = arrayFecha;
             ViewBag.Valor = arrayValor;
             return View();
+        }
+
+        public String getMonth(int num)
+        {
+            String cadena = "";
+            try
+            {
+                switch (num)
+                {
+                    case 1:
+                        cadena = "Enero";
+                        break;
+                    case 2:
+                        cadena = "Febrero";
+                        break;
+                    case 3:
+                        cadena = "Marzo";
+                        break;
+                    case 4:
+                        cadena = "Abril";
+                        break;
+                    case 5:
+                        cadena = "Mayo";
+                        break;
+                    case 6:
+                        cadena = "Junio";
+                        break;
+                    case 7:
+                        cadena = "Julio";
+                        break;
+                    case 8:
+                        cadena = "Agosto";
+                        break;
+                    case 9:
+                        cadena = "Septiembre";
+                        break;
+                    case 10:
+                        cadena = "Octubre";
+                        break;
+                    case 11:
+                        cadena = "Noviembre";
+                        break;
+                    case 12:
+                        cadena = "Diciembre";
+                        break;
+
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return cadena;
+
         }
 
 
